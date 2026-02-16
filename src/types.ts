49f2804b5556f701888dae2046487307fef1c18e
John Lozano <john_lozano2003@yahoo.com>
2026-02-16 11:45:17 -0600

    SEO

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,18 @@
+export type GameFaq = {
+  question: string;
+  answer: string;
+};
+
 export type Game = {
   title: string;
   slug: string;
   description?: string;
   image: string;
+  seoIntro?: string;
+  seoDetails?: string;
+  seoHowToPlay?: string;
+  seoFeatures?: string[];
+  faq?: GameFaq[];
   fullScreenUrl: string | null;
   embedUrl: string | null;
   isComingSoon?: boolean;
